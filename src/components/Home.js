ea817c45ffba7b0d5b68d2174d4f4a9a06b7b14e
Badr CHOUJAI <57566300+BadrChoujai@users.noreply.github.com>
2024-03-30 23:42:50 +0000

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div class="w-full h-screen flex justify-center items-center bg-gradient-to-t from-blue-900 via-fuchsia-400 to-rose-400">
-      <h1 class="text-white text-xl font-bold text-center">Welcome to my portfolio, 
+      <h1 class="text-white text-4xl font-bold w-1/3 text-center">Welcome to my portfolio, 
         🌟🚀 Out of orbit for a bit! 🚀🌟
         🌌 Taking a cosmic break, but I'll be back before you know it! ⏳
         🌠 Keep shining bright and keep the universe in balance until my return! 🌌✨
