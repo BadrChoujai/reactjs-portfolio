c5fdc787db46356b0d398dff26d87a13bc5b02ed
Badr CHOUJAI <57566300+BadrChoujai@users.noreply.github.com>
2024-03-30 23:37:18 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { Contact } from "./components/Contact";
 import { Home } from "./components/Home";
-import { Navbar } from "./components/Navbar";
-import { ErrorPage } from "./components/ErrorPage";
 
 function App() {
   return (
     <div>
-      <Navbar />
       <div className="w-screen h-screen">
         <div className="flex justify-center">
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="contact-me" element={<Contact />} />
-            <Route exact path="*" element={<ErrorPage />} />
           </Routes>
         </div>
       </div>
